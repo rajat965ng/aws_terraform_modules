83da39f0382546d8d89728a7f33c281e7ba3eba8
Rajat Nigam <rajnigam@publicisgroupe.net>
2021-03-03 17:46:10 +0530

    update output file

--- cognito/output.tf
+++ cognito/output.tf
@@ -1,9 +1,9 @@
 output "user_pool_id" {
   description = "Id of user pool"
-  value = "${aws_cognito_user_pool.user_pool.id}"
+  value = "${aws_cognito_user_pool.main.id}"
 }
 
 output "user_pool_client_id" {
   description = "Id of user pool client"
-  value = "${aws_cognito_user_pool_client.user_pool.id}"
+  value = "${aws_cognito_user_pool_client.main.id}"
 }
