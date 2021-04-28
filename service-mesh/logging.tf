47e5673c7ee650a93931febcd2e243af94313039
Rajat Nigam <rajnigam@publicisgroupe.net>
2021-04-28 11:56:15 +0530

    update elk and filebeat

--- service-mesh/logging.tf
+++ service-mesh/logging.tf
@@ -23,3 +23,22 @@ resource "helm_release" "logstash" {
   namespace         = kubernetes_namespace.logging[count.index].metadata.0.name
   depends_on        = [helm_release.elasticsearch]
 }
+
+resource "helm_release" "filebeat" {
+  count             = var.enable_logging ? 1 : 0
+  dependency_update = true
+  name              = "filebeat"
+  chart             = "helm-charts/filebeat"
+  namespace         = kubernetes_namespace.logging[count.index].metadata.0.name
+  depends_on        = [helm_release.logstash]
+}
+
+
+resource "helm_release" "kibana" {
+  count             = var.enable_logging ? 1 : 0
+  dependency_update = true
+  name              = "kibana"
+  chart             = "helm-charts/kibana"
+  namespace         = kubernetes_namespace.logging[count.index].metadata.0.name
+  depends_on        = [helm_release.logstash]
+}
