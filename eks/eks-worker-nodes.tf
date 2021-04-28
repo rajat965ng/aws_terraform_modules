1353a7a1e24b8992cb1093382bc94830ea1f6440
Rajat Nigam <rajnigam@publicisgroupe.net>
2021-04-28 10:43:38 +0530

    update node pool template

--- eks/eks-worker-nodes.tf
+++ eks/eks-worker-nodes.tf
@@ -45,9 +45,9 @@ resource "aws_eks_node_group" "demo" {
   subnet_ids      = aws_subnet.demo[*].id
 
   scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 2
+    desired_size = 5
+    max_size     = 5
+    min_size     = 3
   }
 
   depends_on = [
